95505dbd3655950387c35589763229e892ca652f
Nirmalya <193157261+CodeWithNirmalya@users.noreply.github.com>
2026-08-12 17:47:36 +0530

    Added anagram program

---
+++ Python/Anagram.py
@@ -0,0 +1,5 @@
+'''Example:
+listen & silent → True'''
+a = input("Enter first text to check anagarm:- ").lower()
+b = input("Enter Sec0nd text to check anagarm:-").lower()
+print(f"{a} and {b} ARE anagrams") if sorted(a) == sorted(b) else print(f"{a} and {b} are NOT anagrams")
