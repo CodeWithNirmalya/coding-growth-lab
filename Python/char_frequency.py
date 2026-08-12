01276d144a14baf5e4cdbcbbb676879755b14222
Nirmalya <193157261+CodeWithNirmalya@users.noreply.github.com>
2026-08-12 23:57:26 +0530

    added character frequency program'

---
+++ Python/char_frequency.py
@@ -0,0 +1,11 @@
+s = input("Enter a string: ")
+
+# Dictionary will store character as key and its count as value
+frequency = {}
+
+for ch in s:
+    # If character already exists, increase its count
+    # Otherwise, start its count from 1
+    frequency[ch] = frequency.get(ch, 0) + 1
+
+print(frequency)
