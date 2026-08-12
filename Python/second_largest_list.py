a0657953552d1f6ee9c6f7ff2689131b7ef32176
Nirmalya <193157261+CodeWithNirmalya@users.noreply.github.com>
2026-08-12 17:45:48 +0530

    Added Find second largest element in list program

---
+++ Python/second_largest_list.py
@@ -0,0 +1,4 @@
+# QUESTION --Find second largest number in a list
+listn = [52,41,36,52,5,24,5,465,51,65,135,4685,5,65,498,513,5146,8,316,8496,8,3516,84,8]
+unique_list = sorted(set(listn))
+print(unique_list[-2])
