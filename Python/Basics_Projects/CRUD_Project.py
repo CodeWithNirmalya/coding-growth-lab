d850837fd2c6b01826f8e99035cedf6fdbbc17a9
Nirmalya <193157261+CodeWithNirmalya@users.noreply.github.com>
2026-08-21 07:50:18 +0530

    Added Create File Function

--- Python/Basics_Projects/CRUD_Project.py
+++ Python/Basics_Projects/CRUD_Project.py
@@ -8,3 +8,20 @@ def fileandfolder():
         print(f'{i+1} : {item}')
 print("Your file list in this system are below: ")
 fileandfolder()
+
+#Create Folder Function
+def createfile():
+    try:
+        fileandfolder()
+        name = input("PLease enter your name of the file: ")
+        p= Path(name)   
+        if not p.exists() and  p.is_file():
+            with open(p,'w') as f:
+                data = input("What you want to write inside the file: ")
+                f.write(data)
+
+            print("File created succesfully")
+        else:
+            print("This file is already exist in system")
+    except Exception as err:
+        print(f"You have got an error {err}")
