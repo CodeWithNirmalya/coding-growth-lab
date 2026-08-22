70987a81e4dddfaf29c930faf1b2b41fd69308c8
Nirmalya <193157261+CodeWithNirmalya@users.noreply.github.com>
2026-08-22 23:35:42 +0530

    Extract the length of word using map function

---
+++ Python/Advanced_Topic/length_of_word_from_list.py
@@ -0,0 +1,5 @@
+# QUESTION: Get the length of every word in a list using map().
+
+word = input("Enter a word to check the length:  ")
+word_length = map(lambda x:len(x),word)
+print(f'The length of your word is : {sum(list(word_length))}') 
