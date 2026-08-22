c1139d26c366830a3419081b1c224ef1dc345936
Nirmalya <193157261+CodeWithNirmalya@users.noreply.github.com>
2026-08-22 22:12:13 +0530

    This program will double the value of the number from the given list

---
+++ Python/Advanced_Topic/double_num_in_list.py
@@ -0,0 +1,10 @@
+# QUESTION: Double every number in a list using map() + lambda.
+
+choice = int(input("Enter how much element you wanna push in list: "))
+new_list = []
+for i in range(1,choice+1):
+    num = int(input("Enter the number to add in list: "))
+    new_list.append(num)
+
+double = map(lambda x:x*2,new_list)
+print(f'The double of each number from the given list is: {list(double)}')
