f96e1f90be44a66c1a9aa15deab275751e64e04a
Nirmalya <193157261+CodeWithNirmalya@users.noreply.github.com>
2026-08-27 22:01:27 +0530

    Updated the code solution with pytonic way

--- Python/Advanced_Topic/FILE_HANDLING_Problems/Level-01/read_file.py
+++ Python/Advanced_Topic/FILE_HANDLING_Problems/Level-01/read_file.py
@@ -27,3 +27,25 @@ except FileNotFoundError as err:
     print(f"You got an error while run this program: {err}")
 finally:
     print("\nFile reading operation completed.")
+
+
+
+# Modern Pytonic Way
+try:
+    with open("demo_file.txt", "r") as f:
+        # Read entire file
+        content = f.read()
+        print("Whole file:")
+        print(content)
+
+    with open("demo_file.txt", "r") as f:
+        # Read line by line
+        print("\nLine by line:")
+        for i, line in enumerate(f, start=1):
+            print(f"Line {i}: {line.strip()}")
+
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+
+finally:
+    print("File reading operation completed.")
