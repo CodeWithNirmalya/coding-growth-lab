a4d408f8a2dae9316141bff198e11b8b61294668
CodeWithNirmalya <193157261+CodeWithNirmalya@users.noreply.github.com>
2026-09-13 22:30:48 +0530

    fixed some bugs

--- Python/Interview_Level/list_comparison.py
+++ Python/Interview_Level/list_comparison.py
@@ -10,20 +10,20 @@ print(f'This is the first list: {list_one}')
 for i in range(choice):
     num2 = int(input("Enter the nunbers for the second list: "))
     list_two.append(num2)
-print(f'This is the second list: {list_one}')
+print(f'This is the second list: {list_two}')
 
 
 if (sum(list_one))>(sum(list_two)):
     print(f'This is the list with highest value: {list_one}')    
 elif (sum(list_one))==(sum(list_two)):
-    print("Both list has same value")
+    print(":-:> Both list has same value as per sum of those values")
 else:
     print(f'This is the list with highest value: {list_two}')
 
 
 
 # Take two lists from the user (same length). Print the greater value at each index. 
-print("Greater value at each index:")
+print(":-:> Greater value at each index:")
 
 for i in range(choice):
     if list_one[i]>list_two[i]:
